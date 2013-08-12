79dd2461a2a943e11bdef5d23de1f3cf10c4dfc5
Scrodaddy <bodom@three-wire.com>
2013-08-12 19:27:41 -0400

    Finish Ch 7:  Finish User Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,10 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		# Handle successful save
+  		flash[:success] = "Welcome to the Sample App!"
+  		redirect_to @user
   	else
+  		flash[:error] = "You suck!  What a silly dipshit, can't fill out a form!"
   		render 'new'
   	end
   end
@@ -21,7 +23,7 @@ class UsersController < ApplicationController
   private
 
   	def user_params
-  		params.require(:user).permit(:name, :email, :password,
-  																	:password_confirmation)
+  		params.require(:user).permit(:name, 
+  						:email, :password, :password_confirmation)
   	end
 end
