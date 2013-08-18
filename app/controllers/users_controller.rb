e4dd6274a299de052995689029e5c3e0f5f4f263
Scrodaddy <bodom@three-wire.com>
2013-08-18 14:04:45 -0400

    Fixed code errors, troubleshooting host issue
    1.  added type: request to the authorization describe block in authentication
        pages spec - enables calls to get, pull, patch etc in the spec/features folder
    2.  Fixed typo in the utilities file
    3.  Getting a single failing test for the patch user_path(wrong_user) line.  Message
        states Missing host to link to.  Troubleshooting...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 	attr_accessor :name, :email
-  before_action :signed_in_user, only: [:edit, :update]
+  before_action :signed_in_user,  only: [:edit, :update]
+  before_action :correct_user,    only: [:edit, :update]
 	
   def show
   	@user = User.find(params[:id])
@@ -23,11 +24,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       sign_in @user
@@ -48,5 +47,10 @@ class UsersController < ApplicationController
 
     def signed_in_user
       redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
